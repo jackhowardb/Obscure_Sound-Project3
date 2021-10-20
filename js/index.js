f0018d7f54ac5cc55ed39f99ef2ed3443982fe7c
jkindla <82672312+jkindla@users.noreply.github.com>
2021-10-20 18:17:04 -0500

    edited

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.wrapper > img')
+    .forEach((item, index, it) => {
+      let delay = ~~(Math.random() * 10 - 5)
+      console.log(delay);
+      item.style.left = 150 * (index - 3) + 'px'
+      item.style.animationDelay = `${delay}s`
+    })
